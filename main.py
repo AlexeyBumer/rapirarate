84a65c6ef4ddb34af90e0c29c69abc54c8699e05
AlexeyBumer <zavetniybumer@yandex.ru>
2026-05-17 00:14:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def send_message(text):
     try:
         url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-        requests.post(url, data={
+        requests.post(url, json={
             "chat_id": CHAT_ID,
             "text": text
             "parse_mode": "HTML"
